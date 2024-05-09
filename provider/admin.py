30a32ed404d82beba5570f0d316c67b444a7e166
Anaxuname <anaxuname@gmail.com>
2024-05-09 21:34:47 +0300

    added admin

--- provider/admin.py
+++ provider/admin.py
@@ -1,3 +1,20 @@
-# from django.contrib import admin
+from django.contrib import admin
+from provider.models import Provider, Contact
+from django.db.models import QuerySet
 
-# Register your models here.
+
+@admin.register(Provider)
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = ("name", "provider_type", "parent_provider", "dept")
+    list_filter = ("provider_type",)
+    actions = ["dept_clear"]
+
+    @admin.action(description="Clear dept")
+    def dept_clear(self, request, queryset: QuerySet):
+        queryset.update(dept=0.0)
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("email", "country", "city", "street", "home")
+    list_filter = ("city",)
