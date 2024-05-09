68209e26c715f8bed948adbac2fe2f201157bf24
Anaxuname <anaxuname@gmail.com>
2024-05-09 21:32:08 +0300

    fixed models

--- provider/models.py
+++ provider/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class ProviderType(models.IntegerChoices):
-    Factory = 1, "Завод"
-    Retailer = 2, "Розничная сеть"
-    Entrepreneur = 3, "Индивидуальный предприниматель"
+    Factory = 0, "Завод"
+    Retailer = 1, "Розничная сеть"
+    Entrepreneur = 2, "Индивидуальный предприниматель"
 
 
 class Provider(models.Model):
@@ -13,8 +13,9 @@ class Provider(models.Model):
     )
     parent_provider = models.ForeignKey(
         "self",
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         null=True,
+        blank=True,
         verbose_name="Parent provider",
     )
     provider_type = models.IntegerField(
@@ -22,16 +23,39 @@ class Provider(models.Model):
         verbose_name="Provider type",
     )
     dept = models.DecimalField(
-        max_digits=10, decimal_places=2, null=True, verbose_name="Dept"
+        max_digits=10, decimal_places=2, default=0.0, verbose_name="Dept"
     )
-    update_at = models.DateTimeField(auto_now=True, verbose_name="Update date")
-    create_at = models.DateTimeField(
-        auto_now_add=True, verbose_name="Create date"
+
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Updated date"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Created date"
     )
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "Provider"
-        verbose_name_plural = "Providers"
+        verbose_name = "Поставщик"
+        verbose_name_plural = "Поставщики"
+
+
+class Contact(models.Model):
+    email = models.EmailField(max_length=100, verbose_name="Email")
+    country = models.CharField(max_length=50, verbose_name="Country")
+    city = models.CharField(max_length=50, verbose_name="City")
+    street = models.CharField(max_length=255, verbose_name="Street")
+    home = models.CharField(max_length=50, verbose_name="Home")
+    provider = models.OneToOneField(
+        Provider,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Контакт"
+        verbose_name_plural = "Контакты"
